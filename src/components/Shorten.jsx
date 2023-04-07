04ca551e5281a7d172464580559795a3d7e1f36e
kinghennry <keelechiogbu@gmail.com>
2023-04-07 20:26:55 +0100

    added shorten

--- src/components/Shorten.jsx
+++ src/components/Shorten.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Shorten() {
   const [link, setLink] = useState("");
@@ -12,7 +12,7 @@ function Shorten() {
     setLinkInfo(null);
 
     if (!link) {
-      setStatus({ error: true, info: "Please provide your Link!" });
+      setStatus({ error: true, info: "Please check your link and try again." });
       return;
     }
 
@@ -29,7 +29,7 @@ function Shorten() {
         } else
           setStatus({
             error: true,
-            info: "Please add a valid link!",
+            info: "Please check your link and try again.!",
           });
       })
       .catch((err) => {
@@ -42,7 +42,6 @@ function Shorten() {
       <div className="container">
         <div className="">
           <form className="shorten_form">
-            {/* <div> */}
             <input
               placeholder="Shorten your link"
               title="URL Input"
@@ -51,12 +50,10 @@ function Shorten() {
               name="url"
               value={link}
             />
-            {/* </div> */}
-            {/* <div> */}
+
             <button type="submit" onClick={shortenURL}>
               Shorten
             </button>
-            {/* </div> */}
           </form>
           <p className="shorten_note text-ct">
             By Clicking SHORTEN, you are agreeing to Bitly's&nbsp;
@@ -69,7 +66,8 @@ function Shorten() {
 
         {status?.error && (
           <div className="error">
-            {status.info} <span onClick={() => setStatus(null)}>&times;</span>
+            {status.info}&nbsp;{" "}
+            <span onClick={() => setStatus(null)}>&times;</span>
           </div>
         )}
       </div>
@@ -79,6 +77,19 @@ function Shorten() {
 
 const Shortened = ({ linkInfo }) => {
   const [copied, setCopied] = useState(false);
+  const [isTablet, setIsTablet] = useState(window.innerWidth <= 768);
+
+  const handleResize = () => {
+    setIsTablet(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   const handleCopy = () => {
     try {
       navigator.clipboard
@@ -92,29 +103,39 @@ const Shortened = ({ linkInfo }) => {
 
   return (
     <>
-      <div className="shortened flex-sty">
-        <div className="base_url">
-          <h4>
-            {linkInfo.result.original_link.length > 57
-              ? `${linkInfo.result.original_link.substring(0, 57)}...`
-              : ""}
-          </h4>
-        </div>
-        <div className="shortened_rslt flex-sty">
-          <h4 id="rslt_url" className="rslt_url">
-            {linkInfo.result.full_short_link}
-          </h4>
+      <div className="shortn flex-dir">
+        <div className="shortened flex-sty">
+          <div className="base_url">
+            <h4>
+              {isTablet && linkInfo.result.original_link.length > 40
+                ? `${linkInfo.result.original_link.substring(0, 40)}...`
+                : `${linkInfo.result.original_link.substring(0, 57)}...`}
+            </h4>
+          </div>
+          <div className="shortened_rslt flex-sty">
+            <h4 id="rslt_url" className="rslt_url">
+              {linkInfo.result.full_short_link}
+            </h4>
 
-          {navigator.clipboard !== undefined && (
-            <div
-              //   disabled={copied}
-              onClick={handleCopy}
-              className={`action_btn copy ${copied && "copied"}`}
-            >
-              {copied ? "Copied!" : "Copy"}
-            </div>
-          )}
+            {navigator.clipboard !== undefined && (
+              <div
+                disabled={copied}
+                onClick={handleCopy}
+                className={`action_btn copy ${copied && "copied"}`}
+              >
+                {copied ? "Copied!" : "Copy"}
+              </div>
+            )}
+          </div>
         </div>
+        {linkInfo.result.original_link.length >= 1 ? (
+          <div className="note flex-sty">
+            <h3>Need more redirects, custom back-half links, or QR Codes </h3>
+            <p>Check out our Starter Plan</p>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
--- src/components/Testemonial.jsx
+++ src/components/Testemonial.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import Banner from "./Banner";
 
 function Testemonial() {
+  const items = [
+    {
+      id: 1,
+      text: "Our goal was to implement a system that would not interrupt the gameplay experince when a new update was announced, and we found QR Codes to be the perfect answer",
+      name: "Juan Zacarias – Game Product Manager, Motive Studios EA",
+    },
+    {
+      id: 2,
+      text: "Bitly is user-friendly and accessible. It’s the one-stop-shop for all link-associated things.",
+      name: "Jashima W. – Founder, Ode Brand & Marketing Consultancy",
+    },
+    {
+      id: 3,
+      text: "The more I use Bitly, the more valuable it becomes to me. It cuts my time down because it saves all my information, and it gives me insight into which links are most popular and have the most clicks.",
+      name: "Mary H. – YouTube Influencer",
+    },
+    {
+      id: 4,
+      text: "We found QR Code Generator an easy platform to use and integrate with our information delivery process. We find the system allows fast editing and design that enables us to deliver updated information quickly. Our online delivery of rich, multimedia content is made easy by our partnership with QR Code Generator, and sets out a new, green way forward in tourism.",
+      name: "Frank Willoughby – Owner, Downtown City Maps",
+    },
+    {
+      id: 5,
+      text: "Bitly has enabled us to build trust with clients by giving them branded short links that carry the main message of the link and that carries our brand. Bitly also gives us data analytics on who is clicking our links which helps us carry the clients forward in the advertising process.",
+      name: "Thankdeka M. – Project Manager",
+    },
+  ];
   return (
     <div>
-      <h1>Testemonial</h1>
+      <div className="container">
+        <div className="flexbox-sty flex-dir spacing">
+          <Banner header="What Bitly customers are saying" />
+        </div>
+      </div>
     </div>
   );
 }
